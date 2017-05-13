60be530079b2e7a14dc07b051fad4d1c0db5c8d7
Fernandez Gonzalo <headlins@gmail.com>
2017-05-12 21:45:06 -0300

    initial commit

---
+++ src/main.py
@@ -0,0 +1,66 @@
+import argparse
+from tabulate import tabulate
+from models import Task
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='sarasa')
+    subparsers = parser.add_subparsers(dest="command", help="commands")
+
+    list_parser = subparsers.add_parser('l', help='List tasks')
+
+    add_parser = subparsers.add_parser('a', help='Add tasks')
+    add_parser.add_argument('desc')
+    add_parser.add_argument('-p', type=int, default=0)
+
+    edit_parser = subparsers.add_parser('e', help='Edit tasks')
+    edit_parser.add_argument('id', type=int)
+    edit_parser.add_argument('desc')
+
+    remove_parser = subparsers.add_parser('r', help='Remove tasks')
+    remove_parser.add_argument('id', type=int)
+
+    return parser.parse_args()
+
+
+def list_task():
+    tasks = [[task.id, task.priority, task.desc] for task in Task]
+    headers = ['ID', 'Prior', 'Descripcion']
+    print(tabulate(tasks, headers))
+
+
+def edit_task(task_id, desc):
+    try:
+        t = Task().get(Task.id == task_id)
+        t.desc = desc
+        t.save()
+    except Task.DoesNotExist:
+        print("Task id doesnt exist")
+
+
+def add_task(desc, priority=0):
+    t = Task()
+    t.desc = desc
+    t.priority = priority
+    t.save()
+
+
+def remove_task(task_id):
+    try:
+        t = Task().get(Task.id == task_id)
+        t.delete_instance()
+    except Task.DoesNotExist:
+        print("Task id doesnt exist")
+
+
+if __name__ == '__main__':
+    arguments = parse_arguments()
+
+    if arguments.command == 'a':
+        add_task(arguments.desc, arguments.p)
+    elif arguments.command == 'e':
+        edit_task(arguments.id, arguments.desc)
+    elif arguments.command == 'l' or arguments.command is None:
+        list_task()
+    elif arguments.command == 'r':
+        remove_task(arguments.id)
---
+++ src/models.py
@@ -0,0 +1,33 @@
+import os
+from peewee import SqliteDatabase
+from peewee import Model
+from peewee import CharField
+from peewee import IntegerField
+
+DATABASE_NAME = 'task.db'
+DATABASE_PATH = os.path.join(os.path.dirname(__file__), DATABASE_NAME)
+db = SqliteDatabase(DATABASE_PATH)
+
+
+class Base(Model):
+    class Meta:
+        database = db
+
+
+class Task(Base):
+    desc = CharField()
+    priority = IntegerField(default=0)
+
+    class Meta:
+        order_by = ('-priority',)
+
+
+db.connect()
+
+
+def create_tables():
+    db.create_tables([Task], safe=True)
+
+
+if __name__ == '__main__':
+    create_tables()
